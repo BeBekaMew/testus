06e595fa78f437e04d52cb2f6f9f7921dc6c7ca4
BeBekaMew <149291779+BeBekaMew@users.noreply.github.com>
2026-07-04 20:33:58 +0300

    Add Geometry V2 finalize scaffold

---
+++ addon/geometry_v2_finalize.py
@@ -0,0 +1,16 @@
+"""Geometry V2 - Finalization stage."""
+
+class FinalizeBuilder:
+    """Final processing scaffold."""
+
+    def apply_scale(self, obj):
+        return obj
+
+    def recalculate_normals(self, obj):
+        return obj
+
+    def shade_smooth(self, obj):
+        return obj
+
+    def finalize(self, obj):
+        return obj
